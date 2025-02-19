4e4c674b6e73ada0e3c907dc9bb38c5c2e95e787
MarkS05 <somidomarkallen_bsit@plmun.edu.ph>
2025-02-19 15:12:56 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_02.py
+++ problem_02.py
@@ -1,12 +1,13 @@
-# Somido Mark Allen
+# YOUR NAME
 # ITELEC2
-# Problem Set 01 - Problem 02
+# Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    print("Square the Number Program")
-number = int(input("Enter a number: "))
-print(f"The square of {number} is {(number * number):.2f}")
+    pass  # replace this line with all of your code
 
 if __name__ == "__main__":
     main()
+print("Square the Number Program")
+number = int(input("Enter a number: "))
+print(f"The square of {number} is {(number * number):.2f}")
